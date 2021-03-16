85a36d8e896301375e7d9598f7dfce0c078c37d9
Daniel Espitia <dsespitiac@gmail.com>
2021-03-15 20:07:53 -0500

    imported main font

--- src/assets/styles/GlobalStyles.js
+++ src/assets/styles/GlobalStyles.js
@@ -1,1 +1,21 @@
 import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  margin: 0;
+  padding: 0;
+`;
+
+export const H2 = styled.h2`
+  margin: 0;
+  padding: 0;
+`;
+
+export const H3 = styled.h2`
+  margin: 0;
+  padding: 0;
+`;
+
+export const P = styled.p`
+  margin: 0;
+  padding: 0;
+`;
