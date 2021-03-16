6a50f3d0b1ea3a23e2d4e0c70b921ed827001ca9
Daniel Espitia <dsespitiac@gmail.com>
2021-03-16 12:50:13 -0500

    implemented rest countries API to home

--- src/components/Home/Countries.js
+++ src/components/Home/Countries.js
@@ -1,8 +1,122 @@
-function Countries() {
+import { singleCountry } from '../../dataShape';
+import { H2, P } from '../../assets/styles/GlobalStyles';
+
+import styled from 'styled-components';
+
+export const CountryCardsContainer = styled.div`
+  display: flex;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  flex-flow: row wrap;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 49px;
+  margin-left: 80px;
+  margin-right: 81px;
+`;
+
+export const CountryCard = styled.div`
+  display: inherit;
+  flex-direction: column;
+  height: 336px;
+  width: 263px;
+  margin-bottom: 72px;
+  background-color: #2b3743;
+  border-radius: 8px;
+`;
+
+export const FlagContainer = styled.div`
+  display: inherit;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  background-color: lightblue;
+  height: 220px;
+  width: 100%;
+`;
+
+export const Flag = styled.img`
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  height: 100%;
+  width: 100%;
+  object-fit: none;
+`;
+
+export const InfoContainer = styled.div`
+  display: inherit;
+  flex-direction: column;
+  height: 50%;
+  width: 100%;
+  margin-top: 27px;
+  margin-left: 24px;
+`;
+
+export const CardTitleContainer = styled.div`
+  display: inherit;
+  align-items: flex-start;
+  width: 80%;
+  text-align: left;
+  background-color: pink;
+`;
+
+export const LimitSpan = styled.span`
+  display: inherit;
+  background-color: lightgreen;
+  max-width: 23ch;
+  width: 100%;
+`;
+
+export const CardTitle = styled(H2)`
+  font-size: 1.09em;
+  font-weight: 800;
+`;
+
+export const DataContainer = styled.div`
+  display: inherit;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 16px;
+`;
+
+export const DataText = styled(P)`
+  font-size: 0.89em;
+  font-weight: 300;
+  margin-bottom: 5px;
+`;
+
+function Countries({ countriesArray }) {
   return (
-    <div>
-      <div></div>
-    </div>
+    <CountryCardsContainer>
+      {countriesArray.length < 1 && <p>loading...</p>}
+      {!!countriesArray &&
+        countriesArray.length > 0 &&
+        countriesArray.map(({ name, population, region, capital, flag }) => {
+          return (
+            <CountryCard>
+              <FlagContainer>
+                <Flag src={flag} alt="flag" />
+              </FlagContainer>
+              <InfoContainer>
+                <CardTitleContainer>
+                  <LimitSpan>
+                    <CardTitle>{name}</CardTitle>
+                  </LimitSpan>
+                </CardTitleContainer>
+                <DataContainer>
+                  <DataText>
+                    <strong>Population:</strong> {population}
+                  </DataText>
+                  <DataText>
+                    <strong>Region:</strong> {region}
+                  </DataText>
+                  <DataText>
+                    <strong>Capital:</strong> {capital}
+                  </DataText>
+                </DataContainer>
+              </InfoContainer>
+            </CountryCard>
+          );
+        })}
+    </CountryCardsContainer>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Header from '../parts/Header';
 import SearchBar from '../components/Home/SearchBar';
 import DropdownFilter from '../components/Home/DropdownFilter';
 import Countries from '../components/Home/Countries';
-
-import { singleCountry } from '../dataShape';
-
-import { H2, P } from '../assets/styles/GlobalStyles';
 
 import styled from 'styled-components';
 
@@ -15,7 +14,7 @@ export const PageContainer = styled.div`
   width: 1440px;
   min-height: 100vh;
   width: 100vw;
-  background-color: #212e37;
+  background-color: #202d36;
 `;
 
 export const Main = styled.main`
@@ -32,75 +31,24 @@ export const SearchFilterContainer = styled.div`
   margin-right: 81px;
 `;
 
-export const CountryCardsContainer = styled.div`
-  display: inherit;
-  flex-flow: row wrap;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin-top: 49px;
-  margin-left: 80px;
-  margin-right: 81px;
-`;
-
-export const CountryCard = styled.div`
-  display: inherit;
-  flex-direction: column;
-  height: 336px;
-  width: 263px;
-  margin-bottom: 72px;
-  background-color: #2b3743;
-  border-radius: 8px;
-`;
-
-export const FlagContainer = styled.div`
-  display: inherit;
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-  height: 50%;
-  width: 100%;
-`;
-
-export const Flag = styled.img`
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-  height: 100%;
-  width: 100%;
-`;
-
-export const InfoContainer = styled.div`
-  display: inherit;
-  flex-direction: column;
-  height: 50%;
-  width: 100%;
-  margin-top: 27px;
-  margin-left: 24px;
-`;
-
-export const CardTitleContainer = styled.div`
-  display: inherit;
-`;
-
-export const CardTitle = styled(H2)`
-  font-size: 1.09em;
-  font-weight: 800;
-`;
-
-export const DataContainer = styled.div`
-  display: inherit;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-top: 16px;
-`;
-
-export const DataText = styled(P)`
-  font-size: 0.89em;
-  font-weight: 300;
-  margin-bottom: 5px;
-`;
-
 function Home() {
-  const currentCountry = singleCountry[0];
-  const { name, population, region, capital, flag } = currentCountry;
+  const [countriesArray, setCountriesArray] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      try {
+        const { data } = await axios({
+          method: 'GET',
+          url: 'https://restcountries.eu/rest/v2/all',
+        });
+        setCountriesArray(data);
+        console.log(countriesArray);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    load();
+  }, []);
 
   return (
     <PageContainer>
@@ -110,170 +58,7 @@ function Home() {
           <SearchBar />
           <DropdownFilter />
         </SearchFilterContainer>
-        <CountryCardsContainer>
-          <CountryCard>
-            <FlagContainer>
-              <Flag src={flag} alt="flag" />
-            </FlagContainer>
-            <InfoContainer>
-              <CardTitleContainer>
-                <CardTitle>{name}</CardTitle>
-              </CardTitleContainer>
-              <DataContainer>
-                <DataText>
-                  <strong>Population:</strong> {population}
-                </DataText>
-                <DataText>
-                  <strong>Region:</strong> {region}
-                </DataText>
-                <DataText>
-                  <strong>Capital:</strong> {capital}
-                </DataText>
-              </DataContainer>
-            </InfoContainer>
-          </CountryCard>
-          <CountryCard>
-            <FlagContainer>
-              <Flag src={flag} alt="flag" />
-            </FlagContainer>
-            <InfoContainer>
-              <CardTitleContainer>
-                <CardTitle>{name}</CardTitle>
-              </CardTitleContainer>
-              <DataContainer>
-                <DataText>
-                  <strong>Population:</strong> {population}
-                </DataText>
-                <DataText>
-                  <strong>Region:</strong> {region}
-                </DataText>
-                <DataText>
-                  <strong>Capital:</strong> {capital}
-                </DataText>
-              </DataContainer>
-            </InfoContainer>
-          </CountryCard><CountryCard>
-            <FlagContainer>
-              <Flag src={flag} alt="flag" />
-            </FlagContainer>
-            <InfoContainer>
-              <CardTitleContainer>
-                <CardTitle>{name}</CardTitle>
-              </CardTitleContainer>
-              <DataContainer>
-                <DataText>
-                  <strong>Population:</strong> {population}
-                </DataText>
-                <DataText>
-                  <strong>Region:</strong> {region}
-                </DataText>
-                <DataText>
-                  <strong>Capital:</strong> {capital}
-                </DataText>
-              </DataContainer>
-            </InfoContainer>
-          </CountryCard><CountryCard>
-            <FlagContainer>
-              <Flag src={flag} alt="flag" />
-            </FlagContainer>
-            <InfoContainer>
-              <CardTitleContainer>
-                <CardTitle>{name}</CardTitle>
-              </CardTitleContainer>
-              <DataContainer>
-                <DataText>
-                  <strong>Population:</strong> {population}
-                </DataText>
-                <DataText>
-                  <strong>Region:</strong> {region}
-                </DataText>
-                <DataText>
-                  <strong>Capital:</strong> {capital}
-                </DataText>
-              </DataContainer>
-            </InfoContainer>
-          </CountryCard><CountryCard>
-            <FlagContainer>
-              <Flag src={flag} alt="flag" />
-            </FlagContainer>
-            <InfoContainer>
-              <CardTitleContainer>
-                <CardTitle>{name}</CardTitle>
-              </CardTitleContainer>
-              <DataContainer>
-                <DataText>
-                  <strong>Population:</strong> {population}
-                </DataText>
-                <DataText>
-                  <strong>Region:</strong> {region}
-                </DataText>
-                <DataText>
-                  <strong>Capital:</strong> {capital}
-                </DataText>
-              </DataContainer>
-            </InfoContainer>
-          </CountryCard><CountryCard>
-            <FlagContainer>
-              <Flag src={flag} alt="flag" />
-            </FlagContainer>
-            <InfoContainer>
-              <CardTitleContainer>
-                <CardTitle>{name}</CardTitle>
-              </CardTitleContainer>
-              <DataContainer>
-                <DataText>
-                  <strong>Population:</strong> {population}
-                </DataText>
-                <DataText>
-                  <strong>Region:</strong> {region}
-                </DataText>
-                <DataText>
-                  <strong>Capital:</strong> {capital}
-                </DataText>
-              </DataContainer>
-            </InfoContainer>
-          </CountryCard><CountryCard>
-            <FlagContainer>
-              <Flag src={flag} alt="flag" />
-            </FlagContainer>
-            <InfoContainer>
-              <CardTitleContainer>
-                <CardTitle>{name}</CardTitle>
-              </CardTitleContainer>
-              <DataContainer>
-                <DataText>
-                  <strong>Population:</strong> {population}
-                </DataText>
-                <DataText>
-                  <strong>Region:</strong> {region}
-                </DataText>
-                <DataText>
-                  <strong>Capital:</strong> {capital}
-                </DataText>
-              </DataContainer>
-            </InfoContainer>
-          </CountryCard><CountryCard>
-            <FlagContainer>
-              <Flag src={flag} alt="flag" />
-            </FlagContainer>
-            <InfoContainer>
-              <CardTitleContainer>
-                <CardTitle>{name}</CardTitle>
-              </CardTitleContainer>
-              <DataContainer>
-                <DataText>
-                  <strong>Population:</strong> {population}
-                </DataText>
-                <DataText>
-                  <strong>Region:</strong> {region}
-                </DataText>
-                <DataText>
-                  <strong>Capital:</strong> {capital}
-                </DataText>
-              </DataContainer>
-            </InfoContainer>
-          </CountryCard>
-        </CountryCardsContainer>
+        <Countries countriesArray={countriesArray} />
       </Main>
     </PageContainer>
   );
